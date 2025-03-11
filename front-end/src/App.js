7ccb9b2e4535eff8e5f679b0bcc0c8732a270b6c
huyy422 <ht2354@nyu.edu>
2025-03-11 00:06:03 -0400

    initial home calendar

---
+++ front-end/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import SimpleCalendar from "./SimpleCalendar";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app-container">
+      <SimpleCalendar />
+    </div>
+  );
+}
+
+export default App;
