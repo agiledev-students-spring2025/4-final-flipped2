006e69131339aeda4ee782c77591ab53b14153fd
Frank Su <franksu@Franks-MacBook-Air.local>
2025-03-11 21:41:55 -0400

    sidebar and pomodoro timer added

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,26 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import SimpleCalendar from "./SimpleCalendar";
+import ToDo from "./ToDoPage/ToDo";
+import PomodoroTimer from "./components/PomodoroTimer"; // Import PomodoroTimer
 import "./App.css";
 
 function App() {
   return (
-    <div className="app-container">
-      <SimpleCalendar />
-    </div>
+    <Router>
+      <div className="app-container">
+        <div className="page-links">
+          <Link to="/">Home</Link>
+          <Link to="/todo">To-Do List</Link>
+          <Link to="/pomodoro">Pomodoro Timer</Link> {/* Add this link for Pomodoro */}
+        </div>
+        <Routes>
+          <Route path="/" element={<SimpleCalendar />} />
+          <Route path="/todo" element={<ToDo />} />
+          <Route path="/pomodoro" element={<PomodoroTimer />} /> {/* Add this route */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
