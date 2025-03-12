84a74986ada97ee04f0548635fcc6c20c3e78042
huyy422 <ht2354@nyu.edu>
2025-03-11 21:15:06 -0400

    temp buttons for page nav

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,18 +1,20 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import SimpleCalendar from "./SimpleCalendar";
-import PomodoroTimer from './components/PomodoroTimer';
-import Sidebar from './components/Sidebar';
+import ToDo from "./ToDoPage/ToDo";
 import "./App.css";
 
 function App() {
   return (
     <Router>
       <div className="app-container">
+        <div className="page-links">
+          <Link to="/">Home</Link>
+          <Link to="/todo">To-Do List</Link>
+        </div>
         <Routes>
-          <Route path="/" element={<SimpleCalendar />} /> {/* Main Calendar Page */}
-          <Route path="/sidebar" element={<Sidebar />} /> {/* Sidebar Page */}
-          <Route path="/pomodoro" element={<PomodoroTimer />} /> {/* Optional: Pomodoro Page */}
+          <Route path="/" element={<SimpleCalendar />} />
+          <Route path="/todo" element={<ToDo />} />
         </Routes>
       </div>
     </Router>
