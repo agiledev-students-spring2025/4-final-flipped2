07aa3542b7b3efafc1b929afe9df199bd71d98c9
flaisg <fcs2056@nyu.edu>
2025-03-11 22:11:18 -0400

    pomodoro timer UI

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,10 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import SimpleCalendar from "./SimpleCalendar";
 import ToDo from "./ToDoPage/ToDo";
-<<<<<<< HEAD
 import PomodoroTimer from "./components/PomodoroTimer"; // Import PomodoroTimer
-=======
->>>>>>> origin/master
 import "./App.css";
 
 function App() {
@@ -15,18 +12,12 @@ function App() {
         <div className="page-links">
           <Link to="/">Home</Link>
           <Link to="/todo">To-Do List</Link>
-<<<<<<< HEAD
           <Link to="/pomodoro">Pomodoro Timer</Link> {/* Add this link for Pomodoro */}
-=======
->>>>>>> origin/master
         </div>
         <Routes>
           <Route path="/" element={<SimpleCalendar />} />
           <Route path="/todo" element={<ToDo />} />
-<<<<<<< HEAD
           <Route path="/pomodoro" element={<PomodoroTimer />} /> {/* Add this route */}
-=======
->>>>>>> origin/master
         </Routes>
       </div>
     </Router>
