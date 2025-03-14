56e69f4dbba6e6d2c0e4fe4922f360da81a1abb9
vernairesl <rl4385@nyu.edu>
2025-03-14 04:23:28 -0400

    fix error in addtaskpopup file

--- front-end/src/AddTaskPopup.js
+++ front-end/src/AddTaskPopup.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './AddTaskPopup.css';
+import './AddEditPopup.css';
 
 const AddTaskPopup = ({ onSave }) => {
   const navigate = useNavigate();
