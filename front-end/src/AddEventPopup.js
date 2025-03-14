070b0dcbaa0b8c4b1794c3b043b2a48a244582fb
vernairesl <rl4385@nyu.edu>
2025-03-14 05:19:59 -0400

    update popup js and css for visual inprovement, update Todo and simplecalendar for correct navi to addtask

--- front-end/src/AddEventPopup.js
+++ front-end/src/AddEventPopup.js
@@ -28,49 +28,54 @@ const AddEventPopup = ({ onSave }) => {
       <h2 className="popup-title">Add Event</h2>
       <div className="popup-content">
         <form>
-          <label htmlFor="ename">Event :</label>
-          <input
-            type="text"
-            id="ename"
-            name="ename"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
-          <br /><br />
-          <label htmlFor="date">Date :</label>
-          <input
-            type="text"
-            id="date"
-            name="date"
-            value={date}
-            onChange={(e) => setDate(e.target.value)}
-          />
-          <br /><br />
-          <label htmlFor="time">Time :</label>
-          <input
-            type="text"
-            id="time"
-            name="time"
-            value={time}
-            onChange={(e) => setTime(e.target.value)}
-          />
-          <br /><br />
-          <label htmlFor="duration">Duration :</label>
-          <input
-            type="text"
-            id="duration"
-            name="duration"
-            value={duration}
-            onChange={(e) => setDuration(e.target.value)}
-          />
+          <div className="form-group">
+            <label htmlFor="ename">Event: </label>
+            <input
+              type="text"
+              id="ename"
+              name="ename"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+            <br /><br />
+            <label htmlFor="date">Date: </label>
+            <input
+              type="text"
+              id="date"
+              name="date"
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+            />
+            <br /><br />
+            <label htmlFor="time">Time: </label>
+            <input
+              type="text"
+              id="time"
+              name="time"
+              value={time}
+              onChange={(e) => setTime(e.target.value)}
+            />
+            <br /><br />
+            <label htmlFor="duration">Duration: </label>
+            <input
+              type="text"
+              id="duration"
+              name="duration"
+              value={duration}
+              onChange={(e) => setDuration(e.target.value)}
+            />
+          </div>
         </form>
-        <br /><br />
-        <button type="button" onClick={handleCancel}>
-          Cancel
-        </button>
-        <button type="button" onClick={handleSave}>
-          Save
-        </button>
+        <br />
+        <div className="button-container">
+          <button type="button" className="cancel-button" onClick={handleCancel}>
+            Cancel
+          </button>
+          <button type="button" className="save-button" onClick={handleSave}>
+            Save
+          </button>
+        </div>
+        
       </div>
     </div>
   );
