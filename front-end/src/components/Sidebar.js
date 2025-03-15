a2cdffd1071ef1a99992930caed34b5568075797
flaisg <fcs2056@nyu.edu>
2025-03-15 17:31:21 -0400

    sidebar added

---
+++ front-end/src/components/Sidebar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/todo">To-Do List</Link></li>
+        <li><Link to="/inprogress">In Progress</Link></li>
+        <li><Link to="/done">Done</Link></li>
+        <li><Link to="/pomodoro">Pomodoro Timer</Link></li>
+        <li><Link to="/addevent">Add Event</Link></li>
+        <li><Link to="/addtask">Add Task</Link></li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
