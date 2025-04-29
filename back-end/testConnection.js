b3f853c5787ac3318d8d7d4317ab4f987541d01d
huyy422 <ht2354@nyu.edu>
2025-04-29 18:44:03 -0400

    additonal tests/minor changes

--- back-end/testConnection.js
+++ back-end/testConnection.js
@@ -1,15 +1,21 @@
-// testConnection.js
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 
+mongoose.set('strictQuery', true);
+const uri = process.env.MONGODB_URI;
+if (!uri) {
+  console.error('MONGODB_URI not defined in .env file');
+  process.exit(1);
+}
+
 mongoose
-  .connect(process.env.MONGODB_URI)
+  .connect(uri)
   .then(() => {
-    console.log('MongoDB connected successfully!');
+    console.log(`MongoDB connected to database: ${mongoose.connection.name}`);
     process.exit(0);
   })
   .catch((error) => {
-    console.error('MongoDB connection error:', error);
+    console.error('MongoDB connection error:', error.message);
     process.exit(1);
   });
